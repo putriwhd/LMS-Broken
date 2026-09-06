3fb05fda9e163dcf7a91c7a454219ed817c3c1af
PutuNgurahSemara <ngurahgondol1260@gmail.com>
2026-09-06 17:57:06 +0800

    Redirect root route to dashboard or login

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return auth()->check() ? redirect()->route('dashboard') : redirect()->route('login');
 });
 
 // Guest Routes
